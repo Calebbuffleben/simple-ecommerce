161af1b79acb5d61bf5bcd4d34aafdaa273820f3
Calebbuffleben <calebbuffleben@gmail.com>
2023-07-28 17:06:42 -0300

    add zod

--- simple-ecommerce/src/app/components/FormComponent/FormComponent.tsx
+++ simple-ecommerce/src/app/components/FormComponent/FormComponent.tsx
@@ -1,13 +1,12 @@
 "use client"
-const FormComponent = ({ register, handleSubmit, handleProduct, defaultValues }: any) => {
-  return(
-    <form onSubmit={handleSubmit(handleProduct)}>
-      <input {...register('title')} defaultValue={defaultValues?.title} type="text" placeholder="Título" />
-      <input {...register('description')} defaultValue={defaultValues?.description} type="text" placeholder="Descrição" />
-      <input type="image" />
-      <button type="submit" >Salvar Produto</button>
-    </form>
-  );
-}
+const FormComponent = ({ register, handleSubmit, handleProduct, defaultValues }: any) => (
+  <form onSubmit={handleSubmit(handleProduct)}>
+    <input {...register('title')} defaultValue={defaultValues?.title} type="text" placeholder="Título" />
+    <input {...register('description')} defaultValue={defaultValues?.description} type="text" placeholder="Descrição" />
+    <input type="image" />
+    <button type="submit" >Salvar Produto</button>
+  </form>
+);
+
 
 export default FormComponent;
--- simple-ecommerce/src/app/register-product/page.tsx
+++ simple-ecommerce/src/app/register-product/page.tsx
@@ -2,18 +2,23 @@
 import { SubmitHandler, useForm } from 'react-hook-form'
 import FormComponent from '../components/FormComponent/FormComponent';
 import api from '../services/api'
+import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface IProductValues {
-  title: string;
-  description: string;
-}
+const schema = z.object({
+  title: z.string(),
+  description: z.string().length(200)
+})
+
+type ProductValues = z.infer<typeof schema>;
 
 export default function Register() {
-  const { handleSubmit, register } = useForm<IProductValues>({
-    mode: 'onChange'
+  const { handleSubmit, register } = useForm<ProductValues>({
+    mode: 'onChange',
+    resolver: zodResolver(schema)
   })
 
-  const handleCreateProduct = async (data: IProductValues): Promise<void> => {
+  const handleCreateProduct = async (data: ProductValues): Promise<void> => {
     console.log(data)
 
     const { title, description } = data;
