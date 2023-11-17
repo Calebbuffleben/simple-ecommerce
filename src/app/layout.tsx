dd7db23f816a33fb725a1b269297fede9fbd1194
Calebbuffleben <calebbuffleben@gmail.com>
2023-11-17 18:30:17 -0300

    redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Providers from '@/utils/provider'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Providers from '@/utils/provider';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}><Providers>{children}</Providers></body>
+      <body className={inter.className}><Provider store={store} ><Providers>{children}</Providers></Provider></body>
     </html>
   )
 }
